e1e6cfc282cb68b4617aa1de3f67db0aa8389821
Christopher Turtle <chris@factory3.io>
2017-09-06 20:07:48 +0100

    client-server with actor

--- lib/fik/client.rb
+++ lib/fik/client.rb
@@ -10,7 +10,14 @@ module Fik
       while true do
         printf "> "
         command = gets.chomp
-        response = HTTParty.get("#{@url}/run?command=#{command}")
+        
+        response = if command == "new"
+          puts "Enter name: "
+          name = gets.chomp
+          HTTParty.get("#{@url}/start?name=#{name}")
+        else
+          HTTParty.get("#{@url}/run?name=#{name}&command=#{command}")
+        end
         puts response
       end
     end
--- lib/fik/server.rb
+++ lib/fik/server.rb
@@ -6,17 +6,36 @@ require 'pry-remote'
 module Fik
   FOLDER = File.expand_path("../../../sample", __FILE__)
   
-  class Server < Sinatra::Base  
+  class Server < Sinatra::Base
+    
+    get '/start' do
+      actor = Fik::Models::Actor.new("name" => params[:name], "starting_room" => "kitchen")
+      add_game(actor)
+      "Let the game begin..."
+    end
+    
     get '/run' do
+      game = games[params[:name]]
       game.run(params[:command])
     end
     
-    def game
-      @@game ||= begin
-        config = Fik::Configuration.new(FOLDER).build
-        g = Fik::GameBuilder.new(config).build
-        interface = Fik::Interfaces::Web.new(g)
-      end
+    private
+    
+    def world
+      @@world ||= Fik::World.new(rooms: config["rooms"], items: config["items"])
+    end
+    
+    def config
+      @@config ||= Fik::Configuration.new(FOLDER).build
+    end
+    
+    def add_game(actor)
+      @@games ||= {}
+      @@games[actor.name] = Fik::Interfaces::Web.new(Fik::Game.new(world, actor))
+    end
+    
+    def games
+      @@games
     end
   end
 end
