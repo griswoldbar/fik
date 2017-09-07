3d31ff6ab5500c643ae42a883db1aeeae973313b
Christopher Turtle <chris@factory3.io>
2017-09-07 20:54:36 +0100

    unnecessary files

--- faye.ru
+++
@@ -1,7 +0,0 @@
-lib = File.expand_path("../lib", __FILE__)
-$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-
-require 'faye'
-
-faye_server = Faye::RackAdapter.new(:mount => '/faye', :timeout => 45)
-run faye_server
--- lib/fik/models/actor.rb
+++ lib/fik/models/actor.rb
@@ -7,23 +7,14 @@ module Fik
         @name, @id = attrs["name"], attrs["name"]
         @starting_room = attrs["starting_room"]
         @inventory = []
-        @inbox = []
       end
 
-      def add_message(message)
-        @inbox << message
-      end
-      
       def remove_item(item_id)
         @inventory -= [item_id]
       end
       
       def add_item(item_id)
         @inventory << item_id
-      end
-      
-      def read_messages
-        @inbox.tap { @inbox = [] }
       end
     end
   end
